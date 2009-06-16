db5b489e3836cf88d06b0ee49d921d7f8b933881
Guy M. Allard <allard.guy.m@gmail.com>
2009-06-16 18:59:20 -0400

    Embellish MyData class and tests.

--- TestMyData.rb
+++ TestMyData.rb
@@ -2,61 +2,60 @@ require 'mydata'
 require 'test/unit'
 require 'logger'
 #
-class TestMyColl < Test::Unit::TestCase
+class TestMyData < Test::Unit::TestCase
   #
   def initialize(name)
     super(name)
     @@log = Logger.new(STDOUT)
     @@log.level = Logger::DEBUG
+		@mda = nil
+		@mdb = nil
   end
+	#
+	def setup
+		@mda = MyData.new("A",1)
+		@mdb = MyData.new("B",2)
+	end
+	#
+	def teardown
+		#
+	end
   #
   def test_lt
     @@log.debug "test_lt starts" if @@log.debug?
-    mda = MyData.new("A",1)
-    mdb = MyData.new("B",2)
-	assert_equal(mda < mdb, true)
+		assert_equal(@mda < @mdb, true)
     @@log.debug "test_lt ends" if @@log.debug?
   end
   #
   def test_gt
     @@log.debug "test_gt starts" if @@log.debug?
-    mda = MyData.new("A",1)
-    mdb = MyData.new("B",2)
-	assert_equal(mdb > mda, true)
+		assert_equal(@mdb > @mda, true)
     @@log.debug "test_gt ends" if @@log.debug?
   end
   #
   def test_eq
     @@log.debug "test_eq starts" if @@log.debug?
-    mda = MyData.new("A",1)
-    mdb = MyData.new("B",2)
-	assert_equal(mda == mdb, false)
+		assert_equal(@mda == @mdb, false)
     @@log.debug "test_eq ends" if @@log.debug?
   end
   #
   def test_ne
     @@log.debug "test_ne starts" if @@log.debug?
-    mda = MyData.new("A",1)
-    mdb = MyData.new("B",2)
-	assert_equal(mda != mdb, true)
+		assert_equal(@mda != @mdb, true)
     @@log.debug "test_ne ends" if @@log.debug?
   end
   #
   def test_le
     @@log.debug "test_le starts" if @@log.debug?
-    mda = MyData.new("A",1)
-    mdb = MyData.new("B",2)
-	assert_equal(mda <= mdb, true)
-	assert_equal(mda <= mda, true)
+		assert_equal(@mda <= @mdb, true)
+		assert_equal(@mda <= @mda, true)
     @@log.debug "test_le ends" if @@log.debug?
   end
   #
   def test_ge
     @@log.debug "test_ge starts" if @@log.debug?
-    mda = MyData.new("A",1)
-    mdb = MyData.new("B",2)
-	assert_equal(mdb >= mda, true)
-	assert_equal(mda >= mda, true)
+		assert_equal(@mdb >= @mda, true)
+		assert_equal(@mda >= @mda, true)
     @@log.debug "test_ge ends" if @@log.debug?
   end
 end
--- mydata.rb
+++ mydata.rb
@@ -1,15 +1,23 @@
 #
+require 'logger'
+#
 class MyData
   include Comparable
   attr_reader :sdata, :ndata
   def initialize(sdata, ndata = 123)
     @sdata = sdata
     @ndata = ndata
+    @@log = Logger.new(STDOUT)
+    @@log.level = Logger::DEBUG
   end
+  #
+  # Convert to string.
   #
   def to_s
     "MyData: #@sdata--#@ndata"
   end
+  #
+  # Implement spaceship so Comparable works.
   #
   def <=>(other)
     self.ndata <=> other.ndata
